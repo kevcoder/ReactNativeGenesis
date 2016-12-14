d39f3cb4bcf9dbe09ac516ccbbf62ebffb9c7e2c
kevcoder <kevcodes@gmail.com>
2016-12-14 00:57:04 -0500

    BoilerPlate React-Native

---
+++ src/app/Todo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	TextInput,
+	TouchableOpacity
+} from 'react-native';
+
+export class Todo extends Component{
+	render() {
+		return(
+			<Text> Hello</Text>
+		)
+	}
+}
